e15a854c313d074b11759169ebaa24de9ad8d128
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-03 19:03:16 +0500

    Dom Practise javaScipt

--- alert.js
+++ alert.js
@@ -1,1 +1,10 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Asadullah";
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Asadullah";
+// document.getElementsByTagName("li")[1].style.color = "blue";
+// document.getElementsByClassName("btn")[0].style.color = "red";
+// document.getElementById("title").innerHTML = "Good Bye";
+
+document.querySelectorAll("li.list")[1].style.color = "blue";
+document.querySelector("#title").innerHTML = "Good Bye";
+document.querySelector("button.btn").style.backgroundColor = "yellow";
+document.querySelector("li a").style.color = "red";
+document.querySelector("h1").style.backgroundColor = "aqua"

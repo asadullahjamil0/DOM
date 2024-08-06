e50a017047a72431401fb0fe94e797a30c584e20
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-06 18:09:51 +0500

    Added fucntions with passing parameters in js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,72 +1,48 @@
 let counter = document.querySelectorAll(".drum").length;
-
 for (let index = 0; index < counter; index++) {
 
     document.querySelectorAll(".drum")[index].addEventListener("click", function () {
         let buttonInnerHtml = this.innerHTML;
-        switch (buttonInnerHtml) {
-            case 'w':
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case 'a':
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case 's':
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case 'd':
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case 'j':
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            case 'k':
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-            case 'l':
-                var kick_bass = new Audio("sounds/kick-bass.mp3");
-                kick_bass.play();
-                break;
 
-            default:
-                break;
-        }
-
+        makeSound(buttonInnerHtml);
     });
 }
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key)
+})
 
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case 'j':
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case 'k':
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case 'l':
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Second way to add event listener to all button elements
-// document.querySelector(".set").addEventListener("click", function() {
-//     alert("I got clicked!");
-// });
+        default:
+            break;
+    }
+}
